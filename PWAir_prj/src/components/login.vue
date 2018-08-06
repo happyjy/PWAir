40d00b03e4f0cc099d5103df46b5d0cde7913018
happyjy <okwoyjy@gmail.com>
2018-08-06 20:23:42 +0900

    inti router

---
+++ PWAir_prj/src/components/login.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  ddddlogin
+  <router-link to="/main">aaaaaa</router-link>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- PWAir_prj/src/router/index.js
+++ PWAir_prj/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+import main from '@/components/main'
+import chart from '@/components/chart'
 
 Vue.use(Router)
 
@@ -8,8 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: main
+    },
+    {
+      path: '/chart',
+      name: 'chart',
+      component: chart
     }
   ]
 })
